85d9c9d0defd0a042757f540393f9c1ef4d011b3
lolito <prof.lolito@gmail.com>
2023-02-23 13:57:02 +0200

    Color to Red and updated the logo on the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+    <footer class="p-4 bg-gray-100 rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
     <div class="sm:flex sm:items-center sm:justify-between">
         <a href="https://radblok.onrender.com/" class="flex items-center mb-4 sm:mb-0">
-            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_632/6fa667164144457.63f722cd450d7.png" class="h-24 mr-3" alt="radBlok Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">< span className="rad">rad</span>blok</span>
+            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_632/6fa667164144457.63f722cd450d7.png" class="h-28 mr-3" alt="radBlok Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">< span className="rad">rad</span><span className="">blok</span></span>
         </a>
         <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
@@ -18,7 +18,7 @@ function Footer() {
         </ul>
     </div>
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© <a href="https://radblok.onrender.com/" class="hover:underline">radBlok</a>. All Rights Reserved.
+    <span class="block text-sm text-black sm:text-center dark:text-gray-400">© <a href="https://radblok.onrender.com/" class="hover:underline"><span className="rad">rad</span>Blok by origin<span className="rad">2020</span></a>. All Rights Reserved.
     </span>
 </footer>
   )
