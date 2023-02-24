97e0c408f5b05c1061132cd614066281c07c4e87
lolito <prof.lolito@gmail.com>
2023-02-24 14:22:37 +0200

    Test 2

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loader() {
   return (
-    <div className="bg-black flex items-center justify-center opacity-75 absolute inset-0 z-[10000]">
+    <div className="bg-black flex items-center justify-center opacity-70 absolute inset-0 z-[10000]">
       <div className="h-20 w-20 border-4 border-t-red rounded-full animate-spin border-white"></div>
     </div>
   );
