f0717909800321c71175c81cb69603787109cdd9
lolito <prof.lolito@gmail.com>
2023-03-08 11:22:32 +0200

    Added an sxtra text in loghin

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -62,7 +62,7 @@ function Login() {
           />
 
           <Link to="/register" className="text-center text-primary underline">
-            Don't have an account? Register
+            Don't have an account? Click Here to Register.
           </Link>
         </div>
       </div>
