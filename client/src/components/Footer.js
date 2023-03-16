f91712daa50a51e0290200bf4d3b6955e1ddaa28
lolito <prof.lolito@gmail.com>
2023-03-16 08:54:15 +0200

    Added Image On Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,20 @@ function Footer() {
 
     <div className="max-w-2xl mx-auto text-center">
 
-	<footer className="p-4 bg-white rounded-lg shadow items-center p-6">
+	<footer className="bg-white rounded-lg shadow items-center p-6">
+
+  <div class="flex flex-wrap justify-center">
+  <img
+    src={process.env.PUBLIC_URL + 'logo192.png'}
+    class=" h-15 max-w-sm bg-white p-1 dark:border-neutral-700 dark:bg-neutral-800"
+    alt="logo" />
+    
+</div>
+<h1 className="text-2xl font-bold text-center text-primary">
+            <span className="rad">rad</span>Blok
+          </h1>
+          <br />
+
 		<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 < span className="rad">rad</span><span className="blak">Blok by origin< span className="rad">2020</span> | All Rights Reserved</span>
     </span>
 	</footer>
