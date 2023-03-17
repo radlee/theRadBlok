06111dafd1a26849dbfe580cd5e6015d5502ea4c
lolito <prof.lolito@gmail.com>
2023-03-17 15:02:57 +0200

    Hr at the Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -22,6 +22,7 @@ function Footer() {
 <h1 className="text-2xl font-bold text-center text-primary">
             <span className="rad">rad</span>Blok
           </h1>
+          <hr className='horizon' />
           <br />
 
 		<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 < span className="rad">rad</span><span className="blak">Blok by origin< span className="rad">2020</span> | All Rights Reserved</span>
