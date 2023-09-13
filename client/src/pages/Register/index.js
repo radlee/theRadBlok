c0855d96f41b1f1c203faa0de9a54af857eb9127
lolito <prof.lolito@gmail.com>
2023-09-13 13:56:15 +0200

    Removed Old Logo

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -36,7 +36,8 @@ function Register() {
   return (
     <div className="flex justify-center h-screen items-center reg-bg">
       <div className=" shadow register-white p-5 w-[380px]">
-      <img src={process.env.PUBLIC_URL + 'rblog.png'} alt="register logo"/>
+      <img src={process.env.PUBLIC_URL + 'spin.png'} alt="register logo"/>
+      <br/>
         <div className="flex flex-col gap-5">
           <h1 className="text-2xl font-bold text-center text-primary">
             <span className="rad">rad</span>Blok - Register
