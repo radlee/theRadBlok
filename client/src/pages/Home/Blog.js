8ce1364f1e007f9953ae977bb4bb82cafa1db8b0
lolito <prof.lolito@gmail.com>
2023-11-30 12:13:35 +0200

    Added the URLs - in the img

--- client/src/pages/Home/Blog.js
+++ client/src/pages/Home/Blog.js
@@ -29,7 +29,7 @@ function Blog({ blog }) {
         <h1 className="text-black text-xl font-bold">{blog.title}</h1>
       </div>
       
-      <img className="check" src={`../uploads/${blog.file}`} alt="Current Image" style={{ maxWidth: '100%', height: 'auto' }} />
+      <img className="check" src={`http://radblok.onrender.com/uploads/${blog.file}`} alt="Current Image" style={{ maxWidth: '100%', height: 'auto' }} />
 
 
       <hr />
