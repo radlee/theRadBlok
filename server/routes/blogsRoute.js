b7a494a993a8ebbcedcf61acbd91b1e7e8d4b851
lolito <prof.lolito@gmail.com>
2023-11-30 16:16:55 +0200

    Fixed the extra src

--- server/routes/blogsRoute.js
+++ server/routes/blogsRoute.js
@@ -32,7 +32,7 @@ console.log("The origin :: BlogsRoute.js - " , process.env.ORIGIN)
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     console.log("🚀 ~ file: upload.ts:11 ~ file", process.cwd());
-    cb(null, `${process.cwd()}/src/uploads`)
+    cb(null, `${process.cwd()}/uploads`)
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
