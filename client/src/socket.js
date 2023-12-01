b8fcd6516e9669e3786117a89ccef5c5e4012ec9
lolito <prof.lolito@gmail.com>
2023-12-01 14:44:14 +0200

    commented localhost on socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000"); // Adjust the port accordingly
-// const socket = io("https://radblok.onrender.com"); // Adjust the port accordingly
+// const socket = io("http://localhost:3000"); // Adjust the port accordingly
+const socket = io("https://radblok.onrender.com"); // Adjust the port accordingly
 
 export default socket;
