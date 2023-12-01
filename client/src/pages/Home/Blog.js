b875b109451d77104924904bcba6513707fbf53c
lolito <prof.lolito@gmail.com>
2023-12-01 15:34:07 +0200

    Removed the url before .uploads

--- client/src/pages/Home/Blog.js
+++ client/src/pages/Home/Blog.js
@@ -21,7 +21,7 @@ function Blog({ blog }) {
         <h1 className="text-black text-xl font-bold">{blog.title}</h1>
       </div>
       
-      <img className="check" src={`https://radblok.onrender.com/uploads/${blog.file}`} alt="Current Image" style={{ maxWidth: '100%', height: 'auto' }} />
+      <img className="check" src={`/uploads/${blog.file}`} alt="Current Image" style={{ maxWidth: '100%', height: 'auto' }} />
 
 
       <hr />
