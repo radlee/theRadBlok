fcf0b5aca834f84097bfa80ec3d6ee5ee2de8ab2
lolito <prof.lolito@gmail.com>
2024-02-13 14:56:19 +0200

    The new app

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ const usersRoute = require("./routes/usersRoute");
 const blogsRoute = require("./routes/blogsRoute");
 const blogActionsRoute = require("./routes/blogActionsRoute");
 
-
-app.use(express.json({limit: "50mb", extended: true}))
-app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 50000}))
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
 
 app.use(express.json());
 
@@ -18,35 +19,35 @@ app.use("/api/users", usersRoute);
 app.use("/api/blogs", blogsRoute);
 app.use("/api/blog-actions", blogActionsRoute);
 
-
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4001;
 const server = require("http").createServer(app);
-
-
 
 // socket io
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://radblok.onrender.com"
+        : "http://localhost:3000",
+    methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-    // join room
-    socket.on("join", (userId) => {
-        socket.join(userId);
-    });
+  // join room
+  socket.on("join", (userId) => {
+    socket.join(userId);
+  });
 
-    // listen for new notification
-    socket.on("newNotification", (notification) => {
-        socket.to(notification.userId).emit("newNotification", notification);
-    });
+  // listen for new notification
+  socket.on("newNotification", (notification) => {
+    socket.to(notification.userId).emit("newNotification", notification);
+  });
 });
-
 
 const path = require("path");
 __dirname = path.resolve();
+
 // render deployment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
